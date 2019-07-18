99a7c9fc4d56248f8d8ab0ac4c0ae3e8419508d3
Victor Bargachev <victorb@tonlabs.io>
2019-07-18 16:16:16 +0300

    Removed signatures from contract06-a; Added contract06.txt

--- solidity/contract06-a.sol
+++ solidity/contract06-a.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-// A sample contract demontrating signatures, mappings, structures and intercontract communtications
+// A sample contract demontrating /*signatures,*/ mappings, structures and intercontract communtications
 
 contract IMyContract {
 	function getCredit() public;
@@ -17,32 +17,15 @@ contract MyContract is IMyContract {
 	}
 
 	mapping(address => ContractInfo) m_allowed;
-	uint256 m_ownerPubkey;
 	
-	// TVM helpers
-	function tvm_sender_pubkey() pure private returns (uint256) {}
-	function tvm_logstr(bytes32) pure private {}
-	
-	// External methods
-	
-	constructor() public {
-		uint256 pubkey = tvm_sender_pubkey();
-		tvm_logstr("after_pubkey");
-		//require(pubkey != 0);
-		m_ownerPubkey = pubkey;
-	}
-	
-	// a kind of modificator
-	function ensureOwner() view private {
-		require(tvm_sender_pubkey() == m_ownerPubkey);
-	}
+	// External messages
 	
 	function setAllowance(address anotherContract, uint64 amount) public {
-		ensureOwner();
 		m_allowed[anotherContract].allowed = amount;
+		return;
 	}
 	
-	// Internal methods
+	// Internal messages
 
 	function getCredit() public {
 		IMyContractCallback(msg.sender).getCreditCallback(m_allowed[msg.sender].allowed);
