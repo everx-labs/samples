e958160c103e01664e0dd20d5655bd2b806a55ed
SilkovAlexander <alexander.s@tonlabs.io>
2019-11-11 16:48:48 +0300

    Update contract11 with comments and signature check

--- solidity/contract11-a.sol
+++ solidity/contract11-a.sol
@@ -4,15 +4,31 @@ pragma solidity ^0.5.0;
 
 contract Kamikaze {
 
+	// Runtime functions
 	function tvm_accept() private pure {}
-	
-	modifier alwaysAccept {
+	function tvm_sender_pubkey() private view returns (uint256) {}
+	function tvm_my_public_key() private pure returns (uint256) {}
+
+	// Modifier that allows to accept inbound message only if it was signed with owner's public
+	// key.
+	modifier checkOwnerAndAccept {
+		require(owners_pubkey == tvm_sender_pubkey());
 		tvm_accept(); _;
 	}
 
 	uint m_counter;
 
-	function sendAllMoney(address payable dest_addr) public alwaysAccept {
+	// State variable storing the owner's public key.
+	uint256 owners_pubkey;
+
+	// Constructor saves the owner's public key in a state variable.
+	constructor() public {
+		owners_pubkey = tvm_my_public_key();
+	}
+
+	// Due to the modifier checkOwnerAndAccept function sendAllMoney can be 
+	// called only by the owner of the contract.
+	function sendAllMoney(address payable dest_addr) public checkOwnerAndAccept {
 		selfdestruct(dest_addr);
 	}
 
