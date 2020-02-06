6b4f77d081caf9dd704ef99cef3b0aec143abf10
Dmitry Borisenkov <dmitriy.borisenkov89@gmail.com>
2020-02-06 10:34:57 +0200

    Add gram giver example in C

---
+++ c/example-8-gramgiver/client.c
@@ -0,0 +1,44 @@
+#include "ton-sdk/tvm.h"
+#include "ton-sdk/messages.h"
+#include "ton-sdk/smart-contract-info.h"
+
+#include "client.h"
+
+#define TON_STRUCT_NAME Message_uint64
+#define TON_STRUCT \
+    FIELD_UNSIGNED(rpaddr, 32) \
+    FIELD_UNSIGNED(value, 64)
+#include "ton-sdk/define-ton-struct-header.inc"
+#define TON_STRUCT_NAME Message_uint64
+#define TON_STRUCT \
+    FIELD_UNSIGNED(rpaddr, 32) \
+    FIELD_UNSIGNED(value, 64)
+#include "ton-sdk/define-ton-struct-c.inc"
+
+void getGrams();
+void getGrams_Impl(unsigned value) {}
+
+void build_internal_uint64_message (MsgAddressInt* dest, unsigned data) {
+    build_internal_message_bounce(dest, 0, 0);
+    Message_uint64 msg = {(unsigned) &getGrams, data};
+    __tvm_builder builder = __builtin_tvm_cast_to_builder(__builtin_tvm_getglobal(6));
+    Serialize_Message_uint64_Impl(&builder, &msg);
+    __builtin_tvm_setglobal(6, __builtin_tvm_cast_from_builder(builder));
+}
+
+// Number of remote procedure calls.
+int numCalls_persistent;
+
+// Implementation of the contract's constructor.
+void constructor_Impl () {
+  ACCEPT();
+}
+
+void requestGrams_Impl(MsgAddressInt remoteContractAddr, unsigned value) {
+  ACCEPT();
+  // increment the counter
+  numCalls_persistent++;
+  // call remote contract function with parameter
+  build_internal_uint64_message (&remoteContractAddr, value);
+  send_raw_message (1);
+}
---
+++ c/example-8-gramgiver/giver.c
@@ -0,0 +1,26 @@
+#include "ton-sdk/tvm.h"
+#include "ton-sdk/messages.h"
+#include "ton-sdk/smart-contract-info.h"
+
+// Implementation of the contract's constructor.
+void constructor_Impl () {
+  ACCEPT();
+}
+
+#ifndef NODE_RUN
+#include "client-address-workaround.h"
+#endif
+
+void getGrams_Impl(unsigned value) {
+  ACCEPT();
+#ifdef NODE_RUN
+  // This is how sender's address should be obtained when running on a node.
+  MsgAddressInt sender = get_sender_address();
+#else
+  // And this is a workaround for running the test in a limited environment,
+  // i.e. by means of tvm_linker test command.
+  MsgAddressInt sender = { 0, 0, client_address };
+#endif
+  build_internal_message(&sender, value);
+  send_raw_message(1);
+}
