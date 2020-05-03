560ee284d8769ded1782b70516616486b7e4d853
SilkovAlexander <alexander.s@tonlabs.io>
2020-05-03 19:22:01 +0300

    Fixes after comments

--- solidity/15_MessageReceiver.sol
+++ solidity/15_MessageReceiver.sol
@@ -1,6 +1,6 @@
-pragma solidity >=0.6.0;
+pragma solidity >= 0.6.0;
 
-// the contract calling the remote contract method with parameter
+// Contract that parses the argument passed to his function.
 contract MessageReceiver {
 
 	modifier alwaysAccept {
@@ -11,23 +11,23 @@ contract MessageReceiver {
 	uint counter;
 
 	// Function to be called from another contract. This function gets TvmCell argument and parses data from it.
-	// Modifier "functionID(uint32 id)" allows to set function identifier.
+	// Reserved keyword "functionID" allows to set function identifier manually.
 	function receiveMessage(TvmCell cell) public alwaysAccept functionID(0x12345678) {
-		// Method toSlice allows to convert cell into slice.
+		// Function toSlice allows to convert cell into slice.
 		TvmSlice slice = cell.toSlice();
-		// Method size() returns size of data bits and number of refs in slice.
+		// Function size() returns size of data bits and number of refs in slice.
 		(uint16 bits, uint8 refs) = slice.size();
 		require(bits == 0, 101);
 		require(refs == 1, 102);
-		// Method loadRefAsSlice() allows to load a cell from slice ref and convert it into a slice.
+		// Function loadRefAsSlice() allows to load a cell from slice ref and convert it into a slice.
 		TvmSlice slice2 = slice.loadRefAsSlice();
 		require(slice2.bits() == 267 + 16 + 8 + 11, 103);
-		// Method decode() allows to load arbitrary types from slice.
+		// Function decode() allows to load arbitrary types from slice.
 		(address addr, uint16 val0, uint8 val1) = slice2.decode(address, uint16, uint8);
 		require(addr.value == 123, 104);
 		require(val0 == 123, 105);
 		require(val1 == 123, 106);
-		// Method loadUnsigned() allows to load an unsigned integer of arbitrary bitsize.
+		// Function loadUnsigned() allows to load an unsigned integer of arbitrary bitsize.
 		uint val2 = slice2.loadUnsigned(11);
 		require(val2 == 123, 107);
 		counter++;
--- solidity/15_MessageSender.sol
+++ solidity/15_MessageSender.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.6.0;
+pragma solidity >= 0.6.0;
 
 // Contract that constructs message to call another contract function and sends it.
 contract MessageSender {
@@ -15,7 +15,8 @@ contract MessageSender {
 		// Create an object of TVM type Builder.
 		TvmBuilder builder;
 
-		// Construct an object of type Message X, described in spec.
+		// Construct an object of type Message X, according to the TL-B scheme
+		// described in spec.
 
 		// currencies$_ grams:Grams other:ExtraCurrencyCollection
 		// = CurrencyCollection;
@@ -29,47 +30,57 @@ contract MessageSender {
 		// init:(Maybe (Either StateInit ^StateInit))
 		// body:(Either X ^X) = Message X;
 
-		// Method storeUnsigned() allows to store unsigned integer of arbitrary size in the builder.
-		builder.storeUnsigned(0, 1);		// int_msg_info$0
-		builder.storeUnsigned(1, 1);		// ihr_disabled
-		builder.storeUnsigned(1, 1);		// bounce
-		builder.storeUnsigned(0, 1);		// bounced
+		// Function storeUnsigned() allows to store unsigned integer of arbitrary size in the builder.
+		builder.storeUnsigned(0, 1);		// int_msg_info$0 - constant value
+		builder.storeUnsigned(1, 1);		// ihr_disabled	  - true (currently disabled for TON)
+		builder.storeUnsigned(1, 1);		// bounce         - true (we want this message to bounce to the sender
+											// in case of error)
+		builder.storeUnsigned(0, 1);		// bounced        - false (this message is not bounced)
 
-		builder.storeUnsigned(0,2);		// src:MsgAddress we store addr_none$00
-							// because network will set it for us.
+		builder.storeUnsigned(0, 2);		// src:MsgAddress we store addr_none$00
+											// because blockchain software will replace
+											// it with the current smart-contract address
 
 
-		// Method store() allows to store variable of arbitrary type in the builder.
+		// Function store() allows to store variable of arbitrary type in the builder.
 		builder.store(anotherContract); 	// dest:MsgAddressInt
 
-		builder.storeUnsigned(0x3989680,28);	// value:CurrencyCollection : grams:Grams
+		builder.storeUnsigned(0x3989680, 28);	// value:CurrencyCollection : grams:Grams   - we attach 0.01 grams to the message,
+												// which is equivalent to the 10,000 gas units in the base workchain.
 
-		builder.storeUnsigned(0, 1);		// value:CurrencyCollection : other:ExtraCurrencyCollection
+		builder.storeUnsigned(0, 1);		// value:CurrencyCollection : other:ExtraCurrencyCollection - we store 0, because
+											// we don't attach any other currencies.
+
+		// In the next 4 fields we store zeroes, because blockchain software will replace them
+		// with the correct values after this function finishes execution.
 		builder.storeUnsigned(0, 4);		// ihr_fee:Grams
 		builder.storeUnsigned(0, 4);		// fwd_fee:Grams
-		builder.storeUnsigned(0, 64);		// created_lt:uint64   we store zero value
-							// because network will set it for us.
-		builder.storeUnsigned(0, 32);		// created_at:uint32   we store zero value
-							// because network will set it for us.
+		builder.storeUnsigned(0, 64);		// created_lt:uint64
+		builder.storeUnsigned(0, 32);		// created_at:uint32
 
-		builder.storeUnsigned(0, 1);		// init:(Maybe (Either StateInit ^StateInit))
-		builder.storeUnsigned(0, 1);		// body:(Either X ^X)
+
+		builder.storeUnsigned(0, 1);		// init:(Maybe (Either StateInit ^StateInit)) - we store 0, because we don't attach
+											// initial state of a contract.
+
+		builder.storeUnsigned(0, 1);		// body:(Either X ^X) - we store zero, because body of the message is stored in the
+											// current cell, not in the ref.
 
 		// Filling message body
-		builder.store(uint32(0x12345678));	// store functionID
+		builder.store(uint32(0x12345678));	// store functionID of the receiving contract function to process this message.
 
+		// Attach the cell with fixed value, that will be checked in the receiving contract.
 		TvmBuilder builder2;
 		builder2.store(address.makeAddrStd(0, 123), uint16(123), uint8(123));
 		builder2.storeUnsigned(123, 11);
 
 		TvmBuilder builder3;
 
-		// Method storeRef() allows to store a builder in the ref of the builder.
+		// Function storeRef() allows to store a builder in the ref of the builder.
 		builder3.storeRef(builder2);
 
 		builder.storeRef(builder3);
 
-		// Method toCell() allows to finish builder and convert it to a cell.
+		// Function toCell() allows to finish builder and convert it to a cell.
 		TvmCell message = builder.toCell();
 		tvm.sendrawmsg(message, 1);
 
