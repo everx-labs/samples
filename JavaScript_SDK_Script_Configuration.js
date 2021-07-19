7ce2b8e72f75bb9bc7f956e2ee6e8798203c1ef0
BigGuy573 <48465089+BigGuy573@users.noreply.github.com>
2021-07-18 22:16:57 -0500

    Update JavaScript_SDK_Script_Configuration.js

--- JavaScript_SDK_Script_Configuration.js
+++ JavaScript_SDK_Script_Configuration.js
@@ -1,3 +1,4 @@
+<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
 ```---
 '###"(BigGuy573/samples/master-BigGuy573/blame/BigGuy573/main-JavaScript_SDK_Script_Configuration.js'80670e50524')]['JavaScript_SDK_Script_Configuration-js' push blame to-> raw
 '->('link:]['raw.githubusercontent.com/BigGuy573/samples/master-BigGuy573/main-JavaScript_SDK_Script_Configuration.js/JavaScript_SDK_Script_Configuration.js)' 
