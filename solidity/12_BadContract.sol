94ac832a618fef747bee0e890b9fba31cd6a8668
IgorKoval <igorkoval999@gmail.com>
2022-02-01 20:42:21 +0300

    Upd Solidity samples

--- solidity/12_BadContract.sol
+++ solidity/12_BadContract.sol
@@ -3,8 +3,8 @@ pragma AbiHeader expire;
 
 contract ContractWithBug {
 	// Version of the contract
-	uint public version = 1;
-	uint public value;
+	uint public m_version = 1;
+	uint public m_value;
 
 	constructor() public {
 		// check that contract's public key is set
@@ -15,7 +15,7 @@ contract ContractWithBug {
 	}
 
 	function setValue(uint a, uint b) public checkPubkeyAndAccept {
-		value = a + b; // there is bug. It will be fixed in next version of contract. See 12_NewVersion.sol
+		m_value = a + b; // there is bug. It will be fixed in next version of contract. See 12_NewVersion.sol
 	}
 
 	// Modifier that allows public function to be called only by message signed with owner's pubkey.
@@ -25,19 +25,21 @@ contract ContractWithBug {
 		_;
 	}
 
-	// Function that changes the code of current contract.
-	function setCode(TvmCell newcode) public pure checkPubkeyAndAccept {
+	// Function that changes the code of the contract.
+	function updateContractCode(TvmCell newcode) public view checkPubkeyAndAccept {
 		// Runtime function that creates an output action that would change this
 		// smart contract code to that given by cell newcode.
 		tvm.setcode(newcode);
-		// Runtime function that replaces current code of the contract with newcode.
+		// Runtime function that replaces current code (in register C3) of the contract with newcode.
+		// It needs to call new `onCodeUpgrade` function
 		tvm.setCurrentCode(newcode);
+		// store all state variable to cell
+		TvmCell stateVars = abi.encode(m_version, m_value);
 		// Call function onCodeUpgrade of the 'new' code.
-		onCodeUpgrade();
+		onCodeUpgrade(stateVars);
 	}
 
-	// After code upgrade caused by calling setCode function we may need to do some actions.
-	// We can add them into this function with constant id.
-	function onCodeUpgrade() private pure {
+	// This function will never be called. But it must be defined.
+	function onCodeUpgrade(TvmCell stateVars) private pure {
 	}
 }
